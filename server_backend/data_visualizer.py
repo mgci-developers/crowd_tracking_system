ef945f27440766518a14a72cc00df3c94f4539d5
Bala Venkataraman <balathegreat999@gmail.com>
2023-06-18 17:31:03 -0400

    some edits

--- server_backend/data_visualizer.py
+++ server_backend/data_visualizer.py
@@ -11,9 +11,8 @@ for name in os.listdir("."): # reads through all files and checks if it is a tex
             a = file.readlines() # scans the file
             print(a)
             for k in a:
-                if not k == "log files": # checks if the line is not the header
+                try: # checks if the line is not the header
                     s = k.split()
-<<<<<<< HEAD
                     print(s)
                     times.append(float(s[1])) 
                     pplavgs.append(float(s[0]))
@@ -33,21 +32,7 @@ for name in os.listdir("."): # reads through all files and checks if it is a tex
                 print(pplavgs)
                 plt.title(name)
                 plt.ylabel('Number of people in the room')
-                plt.xlabel('time in hours after start of log')
+                plt.xlabel('time in seconds after start of log')
                 plt.show() # displays graph
             except:
                 pass
-=======
-                    times.append(int(s[1])) 
-                    angles.append(float(s[-1]))
-                    m = min(times) / 60000
-                    for s in range(len(times)): # converts time from unix timestamp format to hours after start of log
-                        ns = times[s] / 60000
-                        ns -= m
-                        times[s] = ns
-                    plt.plot(times, angles) # creates graph in matplotlib
-                    plt.title(name)
-                    plt.ylabel('Number of people in the room')
-                    plt.xlabel('time in hours after start of log')
-                    plt.show() # displays graph
->>>>>>> parent of ccb2918 (Update data_visualizer.py)
