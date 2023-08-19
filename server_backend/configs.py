e19f2c0430a62f0e3f41383a6461fa55942e2a9c
Bala Venkataraman <balathegreat999@gmail.com>
2023-08-19 08:14:55 -0400

    e

--- server_backend/configs.py
+++ server_backend/configs.py
@@ -5,7 +5,7 @@ ipkeys = {}
 # The following lines below need to be edited to configure the system
 # -------------------------------------------------
 
-DISPLAY = True #In the case that this program is being monitored by surveillance personell, the value should be set to True
+DISPLAY = False #In the case that this program is being monitored by surveillance personell, the value should be set to True
 ipkeys["ROOM 244"] = "10.202.41.194" # These are the IP addresses of the cameras, as well as the name of the location (displayed to the users). add more entries for more camears. This is in the case that the system runs with static IPs.
 
 
